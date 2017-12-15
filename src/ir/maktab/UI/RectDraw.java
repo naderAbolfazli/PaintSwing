51898d9fadf7bc404f271d0dcced0c14a3c84aed
NaderAbolfazli <abolfazli.nader@gmail.com>
2017-12-15 21:26:49 +0330

    completing drawing part

--- src/ir/maktab/UI/RectDraw.java
+++ src/ir/maktab/UI/RectDraw.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
@@ -29,18 +30,14 @@ public class RectDraw extends JFrame {
         getContentPane().setBackground(new Color(186, 186, 186));
         getContentPane().setLayout(null);
         setResizable(false);
+        Font myFont = new Font(Font.SERIF, Font.BOLD, 14);
+
 
         setSize(650, 550);
         setTitle("Paint");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
-        /*DrawPanel dpnl = new DrawPanel();
-        dpnl.setOpaque(true);
-        dpnl.setBackground(new Color(255, 255, 255));
-        dpnl.setBounds(5, 5, 505, 505);
-        add(dpnl);*/
 
         PaintSurface paintS = new PaintSurface();
         //paintS.setOpaque(true);
@@ -49,30 +46,36 @@ public class RectDraw extends JFrame {
         add(paintS);
 
         Button line = new Button("خط");
+        line.setFont(myFont);
         line.setBounds(525, 25, 100, 25);
         line.setActionCommand("line");
         line.addActionListener(e -> ActionPerformed(e));
         add(line);
 
         Button oval = new Button("دایره");
+        oval.setFont(myFont);
         oval.setBounds(525, 75, 100, 25);
         oval.setActionCommand("oval");
         oval.addActionListener(e -> ActionPerformed(e));
         add(oval);
 
         Button rectangle = new Button("مستطیل");
+        rectangle.setFont(myFont);
         rectangle.setBounds(525, 125, 100, 25);
         rectangle.setActionCommand("rectangle");
         rectangle.addActionListener(e -> ActionPerformed(e));
         add(rectangle);
 
         Label label = new Label("انتخاب رنگ");
+        label.setFont(myFont);
         label.setBounds(525, 175, 100, 25);
         label.setAlignment(Label.CENTER);
         add(label);
 
         JRadioButton black = new JRadioButton("مشکی");
+        black.setFont(myFont);
         black.setBounds(525, 225, 100, 25);
+        black.setBackground(Color.GRAY);
         black.setActionCommand("black");
         black.addActionListener(e -> ActionPerformed(e));
         black.setHorizontalAlignment(SwingConstants.CENTER);
@@ -80,21 +83,27 @@ public class RectDraw extends JFrame {
         add(black);
 
         JRadioButton red = new JRadioButton("قرمز");
+        red.setFont(myFont);
         red.setBounds(525, 275, 100, 25);
+        red.setBackground(Color.RED);
         red.setActionCommand("red");
         red.addActionListener(e -> ActionPerformed(e));
         red.setHorizontalAlignment(SwingConstants.CENTER);
         add(red);
 
         JRadioButton green = new JRadioButton("سبز");
+        green.setFont(myFont);
         green.setBounds(525, 325, 100, 25);
+        green.setBackground(Color.GREEN);
         green.setActionCommand("green");
         green.addActionListener(e -> ActionPerformed(e));
         green.setHorizontalAlignment(SwingConstants.CENTER);
         add(green);
 
         JRadioButton blue = new JRadioButton("آبی");
+        blue.setFont(myFont);
         blue.setBounds(525, 375, 100, 25);
+        blue.setBackground(Color.BLUE);
         blue.setActionCommand("blue");
         blue.addActionListener(e -> ActionPerformed(e));
         blue.setHorizontalAlignment(SwingConstants.CENTER);
@@ -107,6 +116,7 @@ public class RectDraw extends JFrame {
         group.add(blue);
 
         Button logOut = new Button("خروج");
+        logOut.setFont(myFont);
         logOut.setBounds(525, 450, 100, 25);
         add(logOut);
 
@@ -152,8 +162,10 @@ public class RectDraw extends JFrame {
                 }
 
                 public void mouseReleased(MouseEvent e) {
-                    Shape r = makeRectangle(startDrag.x, startDrag.y, e.getX(), e.getY());
-                    shapes.put(r, selectedColor);
+                    Shape shape = selectedShape.equals("rectangle") ? makeRectangle(startDrag.x, startDrag.y, endDrag.x, endDrag.y)
+                            : selectedShape.equals("line") ? makeLine(startDrag.x, startDrag.y, endDrag.x, endDrag.y)
+                            : makeOval(startDrag.x, startDrag.y, endDrag.x, endDrag.y);
+                    shapes.put(shape, selectedColor);
                     startDrag = null;
                     endDrag = null;
                     repaint();
@@ -170,6 +182,7 @@ public class RectDraw extends JFrame {
 
         private void paintBackground(Graphics2D g2) {
             g2.setPaint(selectedColor);
+
             for (int i = 0; i < getSize().width; i += 10) {
                 Shape line = new Line2D.Float(i, 0, i, getSize().height);
                 g2.draw(line);
@@ -185,6 +198,7 @@ public class RectDraw extends JFrame {
         public void paint(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(4));
 
             for (Map.Entry<Shape, Color> s : shapes.entrySet()) {
                 g2.setPaint(s.getValue());
@@ -193,36 +207,24 @@ public class RectDraw extends JFrame {
 
             if (startDrag != null && endDrag != null) {
                 g2.setPaint(selectedColor);
-                Shape r = makeRectangle(startDrag.x, startDrag.y, endDrag.x, endDrag.y);
-                g2.draw(r);
+
+                Shape shape = selectedShape.equals("rectangle") ? makeRectangle(startDrag.x, startDrag.y, endDrag.x, endDrag.y)
+                        : selectedShape.equals("line") ? makeLine(startDrag.x, startDrag.y, endDrag.x, endDrag.y)
+                        : makeOval(startDrag.x, startDrag.y, endDrag.x, endDrag.y);
+                g2.draw(shape);
             }
+        }
+
+        private Shape makeOval(int x1, int y1, int x2, int y2) {
+            return new Ellipse2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2)*2, Math.abs(y1 - y2)*2);
+        }
+
+        private Shape makeLine(int x, int y, int x1, int y1) {
+            return new Line2D.Float(x, y, x1, y1);
         }
 
         private Rectangle2D.Float makeRectangle(int x1, int y1, int x2, int y2) {
             return new Rectangle2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
-        }
-    }
-
-    class DrawPanel extends JPanel {
-
-        private void doDrawing(Graphics g) {
-
-            Graphics2D g2d = (Graphics2D) g;
-
-            g2d.setColor(new Color(19, 19, 19));
-            g2d.drawRect(0, 0, 500, 500);
-            g2d.drawOval(300, 300, 20, 20);
-
-            g2d.setColor(new Color(125, 167, 116));
-            g2d.fillRect(10, 15, 90, 60);
-
-        }
-
-        @Override
-        public void paintComponent(Graphics g) {
-
-            super.paintComponent(g);
-            doDrawing(g);
         }
     }
 
